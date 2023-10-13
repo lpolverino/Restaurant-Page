3e2ebb5eb59d95a5723701d9b154e0aefaf7c48c
lpolverino <lorenz.polverino@gmail.com>
2023-10-13 12:40:23 -0300

    Added tab switches to the index script

--- src/index.js
+++ src/index.js
@@ -1,23 +1,53 @@
 import createHome from "./home";
+import createMenu from "./menu";
+import createContact from "./contact"
 
-function createButton() {
+function clearContent(){
+    const oldDiv = document.getElementById("content");
+    const newDiv = document.createElement("div");
+    newDiv.id = "content"
+    oldDiv.replaceWith(newDiv)
+}
+
+function tabChangeHandler(tabCreator){
+    clearContent();
+    tabCreator();
+}
+
+function createButton(eventHandler,title) {
     const button = document.createElement("button");
-    button.innerText = "Click Me"
-
-    return button
+    button.innerText = title
+    button.addEventListener("click", eventHandler);
+    return button;
 }
 
 
-function createHeader(){
+(function createHeader(){
+    const headerDiv = document.getElementsByClassName("header")[0];
     const header = document.createElement("div");
-    const button = createButton();
-
     header.classList.add("header");
-    header.appendChild(button);
 
-    return header
-}
 
-createHeader();
+    const menuButton = createButton( () => {
+        clearContent();
+        tabChangeHandler(createMenu)
+    },"Menu");
+
+    const contactButton = createButton( () => {
+        clearContent();
+        tabChangeHandler(createContact)
+    },"Contact");
+
+    const homeButton = createButton( () => {
+        clearContent();
+        tabChangeHandler(createHome)
+    },"Home");
+
+    header.appendChild(homeButton);
+    header.appendChild(menuButton);
+    header.appendChild(contactButton);
+
+    headerDiv.appendChild(header)
+})();
 
 createHome();
