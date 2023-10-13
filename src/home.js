6c9c0db6567fe5b458bd43a343857dac419e18f2
lpolverino <lorenz.polverino@gmail.com>
2023-10-13 11:37:12 -0300

    refactor to home tab

--- src/home.js
+++ src/home.js
@@ -1,19 +1,41 @@
-export default function createHome(){
-    const contEl = document.getElementById("content");
-    console.log(document);
-
-    const header = document.createElement("div");
+function createButton() {
     const button = document.createElement("button");
-
     button.innerText = "Click Me"
+
+    return button
+}
+
+function createHeader(){
+    const header = document.createElement("div");
+    const button = createButton();
+
     header.classList.add("header");
     header.appendChild(button);
 
+    return header
+}
+
+function createImage(){
+
     const img = document.createElement("img");
     img.src = "../img/facturas.png"
+    return img
+}
 
+function createText(){
     const text = document.createElement("p");
     text.innerText = "We love cooking facturas"
+    return text
+}
+
+export default function createHome(){
+    const contEl = document.getElementById("content");
+
+    const header = createHeader();
+
+    const img = createImage();
+
+    const text = createText();
 
     contEl.appendChild(header)
     contEl.appendChild(img)
